692b19d7cabafc64d8bf84a51653e9d72fece315
cody.koscielski <cody.koscielski@outlook.com>
2020-04-20 20:04:31 -0400

    Add React Native Navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,33 +1,66 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
-import CampstieInfo from './CampsiteInfoComponent';
+import CampsiteInfo from './CampsiteInfoComponent';
+import Home from './HomeComponent';
 
-import { View } from 'react-native';
-import { CAMPSITES } from '../shared/campsites';
+import { View, Platform} from 'react-native';
+import { createStackNavigator, createDrawerNavigator } from 'react-navigation';
+
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    }, 
+    {
+        initialRouteName: 'Directory',
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#2c4943'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const HomeNavigator = createStackNavigator (
+    {
+        Home: { screen: Home},
+    }, 
+    {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#2c4943'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator }
+    },
+    {
+        drawerBackgroundColor: '#315545',
+    }
+);
 
 class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          campsites: CAMPSITES,
-          selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsiteId) {
-        this.setState({ selectedCampsite: campsiteId });
-    }
 
     render() {
         return (
-            <View style={{ flex: 1}}>
-                <Directory 
-                    campsites={this.state.campsites} 
-                    onPress= {campsiteId => this.onCampsiteSelect(campsiteId)}
-                />
-                <CampstieInfo
-                    campsite={ this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]}
-                />
+            <View style={{ 
+                flex: 1,
+                paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight 
+            }}>
+                <MainNavigator />
             </View>
         );
     }
