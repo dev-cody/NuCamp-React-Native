2247c10b742ad64a2b7d654364cc6bdffe4a3a50
cody.koscielski <cody.koscielski@outlook.com>
2020-04-29 19:59:28 -0400

    Finished configuring Redux

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Main from './components/MainComponent';
-
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/ConfigureStore';
 import { YellowBox } from 'react-native';
 
 YellowBox.ignoreWarnings(['Warning: ...'])
 console.disableYellowBox = true;
 
+const store = ConfigureStore();
+
 export default function App() {
     return (
-        <Main />
+        <Provider store={store}>
+            <Main />
+        </Provider>
     );
 }
